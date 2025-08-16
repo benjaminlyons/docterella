ab1a47b17e623aa115605f12cf91a349ea996422
Ben Lyons <docterella@mail.blyons.me>
2025-08-15 21:15:51 -0400

    Refactor benchmarking script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,27 +1,149 @@
+import pandas as pd
+import dataclasses
+import json
+
+from dataclasses import dataclass
+from typing import List
+
 from docterella.connections.base_connection import BaseConnection
 from docterella.connections.anthropic_connection import AnthropicConnection
 from docterella.connections.ollama_connection import OllamaConnection
-from docterella.agent import ValidationAgent
+from docterella.validators.agent import ValidationAgent
 from docterella.parsers.file_parser import FileParser
-from docterella.parsers.sequence_parser import SequenceParser
-from docterella.components.metadata import ClassMetadata
-from docterella.components.metadata import FunctionMetadata
-from docterella.components.results import ValidationResults
 from docterella.components.assessment import ClassDocstringAssessment
 from docterella.components.assessment import FunctionDocstringAssessment
 from docterella.components.assessment import ClassDocstring
 from docterella.components.assessment import FunctionDocstring
 from docterella.components.assessment import ReturnValue
-
-import json
-import pandas as pd
-
-from typing import List
 from docterella.components.assessment import Argument
+from docterella.runner import Runner
 
-import dataclasses
-from dataclasses import dataclass
+@dataclass
+class DocstringArgumentsComparison:
+    arg_name: int = 0
+    arg_data_type: int = 0
+    arg_description: int = 0
+    num_args: int = 0
 
+@dataclass
+class ArgumentComparison:
+    name: int
+    data_type: int
+    description: int
+
+@dataclass
+class ReturnValueComparison:
+    ret_data_type: int
+    num_rets: bool
+
+def main():
+    """
+    Main function to run the benchmarking process.
+
+    This function sets up and runs the benchmarking process using a specified model.
+    """
+    models = [
+        "claude-3-5-haiku-20241022",
+        "llama3.1:8b-instruct-q8_0",
+        "phi3:14b-medium-128k-instruct-q4_K_M",
+        "mistral:7b-instruct-q8_0",
+        "phi4-mini-reasoning:3.8b",
+    ]
+
+    for model in models: 
+        print(f"Running benchmarks for {model}")
+        benchmark(model)
+
+def benchmark(model: str):
+    """
+    Benchmarks a model by comparing its output to expected responses.
+
+    Args:
+        model (str): The name of the model to benchmark.
+    """
+    functions_path = "tests/data/functions.py"
+    functions_response_path = "tests/data/function_responses.json"
+
+    class_path = "tests/data/class.py"
+    class_response_path = "tests/data/class_responses.json"
+
+    paths = [
+        ("function", functions_path, functions_response_path),
+        ("class", class_path, class_response_path),
+    ]
+
+    connection = load_model(model)
+
+    metrics = []
+    for case, input_path, response_path in paths:
+        metric, response = _benchmark_helper(connection, input_path, response_path)
+        metric = metric.rename({col: f"{col}_{case}" for col in metric.index})
+        metrics.append(metric)
+
+    metrics_series = pd.concat(metrics)
+
+    save_model_response(model, response, metrics_series)
+
+def load_model(model):
+    """
+    Loads a model based on the provided name.
+
+    Args:
+        model (str): The name of the model to load.
+
+    Returns:
+        BaseConnection: The loaded model connection.
+    """
+    if "claude" in model:
+        return AnthropicConnection(model)
+    else:
+        return OllamaConnection(model)
+
+def _benchmark_helper(
+    connection: BaseConnection, 
+    input_file_path: str, 
+    response_file_path: str
+):
+    """
+    Runs benchmark a model given an input file and an expected output file
+
+    Args:
+        connection (BaseConnection): The model connection to use.
+        input_file_path (str): The path to the input file containing metadata nodes.
+        response_file_path (str): The path to the expected responses file.
+
+    Returns:
+        pd.Series, str: The metrics series and the generated responses.
+    """
+    with open(response_file_path, 'r') as f:
+        expected_response_dict = json.load(f)
+    
+    validator = ValidationAgent(connection)
+    parser = FileParser(input_file_path)
+
+    runner = Runner(parser, validator)
+
+    metrics = []
+    responses = []
+    for result in runner.validate_sequence():
+        expected = expected_response_dict[result.metadata.name]
+
+        if isinstance(result.assessment, ClassDocstringAssessment):
+            expected_assessment = ClassDocstringAssessment.model_validate(expected)
+        else:
+            expected_assessment = FunctionDocstringAssessment.model_validate(expected)
+
+        assessment = result.assessment
+
+        check = compare_assessments(assessment, expected_assessment)
+        check["name"] = result.metadata.name
+
+        metrics.append(check)
+        responses.append(str(result))
+
+    metrics_series = pd.DataFrame(metrics).mean(numeric_only=True)
+
+    return metrics_series, responses
 
 def compare_assessments(assessment, expected_assessment):
     act_dict = assessment.model_dump()
@@ -48,23 +170,6 @@ def compare_class_docstring(cd1: ClassDocstring, cd2: ClassDocstring):
         )
     )
 
-@dataclass
-class DocstringArgumentsComparison:
-    arg_name: int = 0
-    arg_data_type: int = 0
-    arg_description: int = 0
-    num_args: int = 0
-
-@dataclass
-class ArgumentComparison:
-    name: int
-    data_type: int
-    description: int
-
-@dataclass
-class ReturnValueComparison:
-    ret_data_type: int
-    num_rets: bool
 
 def compare_all_arguments(args1: List[Argument], args2: List[Argument]):
     arg_comp = DocstringArgumentsComparison(
@@ -118,105 +223,6 @@ def compare_return_values(ret1: List[ReturnValue], ret2: List[ReturnValue]):
         num_rets=len(ret1) == len(ret2)
     )
 
-def validate_sequence(parser: SequenceParser, validator: ValidationAgent):
-    for node in parser.parse():
-        if isinstance(node, ClassMetadata):
-            yield validator.validate_class(node)
-        
-        if isinstance(node, FunctionMetadata):
-            yield validator.validate_function(node)
-
-def benchmark(model: str):
-
-    """
-    Benchmarks a model by comparing its output to expected responses.
-
-    Args:
-        model (str): The name of the model to benchmark.
-    """
-    functions_path = "tests/data/functions.py"
-    functions_response_path = "tests/data/function_responses.json"
-
-    class_path = "tests/data/class.py"
-    class_response_path = "tests/data/class_responses.json"
-
-    paths = [
-        ("function", functions_path, functions_response_path),
-        ("class", class_path, class_response_path),
-    ]
-
-    connection = load_model(model)
-
-    metrics = []
-    responses = ""
-    for case, input_path, response_path in paths:
-        metric, response = _benchmark_helper(connection, input_path, response_path)
-        metric = metric.rename({col: f"{col}_{case}" for col in metric.index})
-        metrics.append(metric)
-
-    metrics_series = pd.concat(metrics)
-
-    save_model_response(model, response, metrics_series)
-
-def load_model(model):
-    """
-    Loads a model based on the provided name.
-
-    Args:
-        model (str): The name of the model to load.
-
-    Returns:
-        BaseConnection: The loaded model connection.
-    """
-    if "claude" in model:
-        return AnthropicConnection(model)
-    else:
-        return OllamaConnection(model)
-
-def _benchmark_helper(
-    connection: BaseConnection, 
-    input_file_path: str, 
-    response_file_path: str
-):
-    """
-    Helper function for benchmarking a model.
-
-    Args:
-        connection (BaseConnection): The model connection to use.
-        input_file_path (str): The path to the input file containing metadata nodes.
-        response_file_path (str): The path to the expected responses file.
-
-    Returns:
-        pd.Series, str: The metrics series and the generated responses.
-    """
-    with open(response_file_path, 'r') as f:
-        expected_response_dict = json.load(f)
-    
-    validator = ValidationAgent(connection)
-    parser = FileParser(input_file_path)
-
-    metrics = []
-    responses = []
-    for result in validate_sequence(parser, validator):
-        expected = expected_response_dict[result.metadata.name]
-
-        if isinstance(result.assessment, ClassDocstringAssessment):
-            expected_assessment = ClassDocstringAssessment.model_validate(expected)
-        else:
-            expected_assessment = FunctionDocstringAssessment.model_validate(expected)
-
-        assessment = result.assessment
-
-        check = compare_assessments(assessment, expected_assessment)
-        check["name"] = result.metadata.name
-
-        metrics.append(check)
-        responses.append(str(result))
-
-    metrics_series = pd.DataFrame(metrics).mean(numeric_only=True)
-
-    return metrics_series, responses
-
 def save_model_response(model: str, response: str, metrics: pd.Series, base_path = "tests/data/results/"):
     """
     Saves the model response and metrics to files.
@@ -254,26 +260,6 @@ def model_to_path(model: str):
     translation = str.maketrans({p: "_" for p in punctuation})
 
     return model.translate(translation)
-
-def main():
-    """
-    Main function to run the benchmarking process.
-
-    This function sets up and runs the benchmarking process using a specified model.
-    """
-    models = [
-        "llama3.1:8b-instruct-q8_0",
-        "phi4:latest",
-        "phi3:14b-medium-128k-instruct-q4_K_M",
-        "claude-3-5-haiku-20241022",
-        "mistral:7b-instruct-q8_0",
-        "gemma3:12b-it-qat",
-        "phi4-mini-reasoning:3.8b",
-    ]
-
-    for model in models: 
-        print(f"Running benchmarks for {model}")
-        benchmark(model)
         
 if __name__ == "__main__":
     main()
---
+++ src/docterella/runner.py
@@ -0,0 +1,17 @@
+from docterella.parsers.sequence_parser import SequenceParser
+from docterella.validators.agent import ValidationAgent
+from docterella.components.metadata import ClassMetadata
+from docterella.components.metadata import FunctionMetadata
+
+class Runner:
+    def __init__(self, parser: SequenceParser, agent: ValidationAgent):
+        self.parser = parser
+        self.agent = agent
+
+    def validate_sequence(self):
+        for node in self.parser.parse():
+            if isinstance(node, ClassMetadata):
+                yield self.agent.validate_class(node)
+
+            if isinstance(node, FunctionMetadata):
+                yield self.agent.validate_function(node)
