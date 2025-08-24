019226c4445593c31a51a33fa36f48d7cb162455
Ben Lyons <docterella@mail.blyons.me>
2025-08-24 15:43:19 -0400

    Add cli options to format_outputs script

--- scripts/format_outputs.py
+++ scripts/format_outputs.py
@@ -3,14 +3,25 @@
 import json
 import pandas as pd
 import sys
+import click
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    filename = "tests/data/results/metrics.csv"
+@click.command()
+@click.argument('filename', type=click.Path(exists=True))
+@click.option('--style', '-s', multiple=True, type=str,
+              help='Filter metrics by the style column')
+@click.option('--suite', '-t', multiple=True, type=str,
+              help='Filter metrics by the suite column')
+def main(filename, style, suite):
+    """filename: Path to csv file containing metrics"""
+    df = pd.read_csv(filename)
 
-df = pd.read_csv(filename)
+    if style:
+        df = df[df["style"].isin(style)]
 
-df = df[df["style"] == "reasoning"]
+    if suite:
+        df = df[df["suite"].isin(suite)]
 
-print(json.dumps(df.to_dict(orient='index'), indent=4))
+    print(json.dumps(df.to_dict(orient='index'), indent=4))
+
+if __name__ == "__main__":
+    main()
